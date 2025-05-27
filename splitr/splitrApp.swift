b6c2053fb04fa032418a1b801e53892b45bdcb45
adirathodd <88114611+adirathodd@users.noreply.github.com>
2025-05-26 22:16:04 -0400

    Initial Commit

---
+++ splitr/splitrApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitrApp.swift
+//  splitr
+//
+//  Created by Adityasinh Rathod on 5/26/25.
+//
+
+import SwiftUI
+
+@main
+struct splitrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
